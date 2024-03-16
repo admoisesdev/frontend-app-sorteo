aca9d28eb4e0b4c6e32a223905b77b97c733b93d
JKmoises <moi.prado@duocuc.cl>
2024-03-16 18:58:10 -0300

    Reutilizar componente header en pagina de sorteos

--- src/app/sorteos/page.tsx
+++ src/app/sorteos/page.tsx
@@ -8,7 +8,6 @@ import { useEffect } from 'react';
 
 import useAuth from '@/hooks/useAuth';
 import { CardOption } from '@/components/ui/CardOption';
-import { Header } from '@/components/Header';
 import SorteosContainer from '@/components/ui/SorteosContainer';
 
 const Sorteos = () => {
@@ -18,39 +17,7 @@ const Sorteos = () => {
 	useEffect(() => {
 		if (!isAuthenticated) router.push('/');
 	}, [isAuthenticated, router]);
-  {/* <div className="w-full min-h-screen bg-hero-admin bg-cover bg-[80%_80%] flex flex-col gap-8 ">
-      <Header />
-
-      <main className="w-full min-h-[calc(100vh_-_10rem)] flex items-center justify-center p-4">
-        <section
-          className="w-full grid grid-cols-[repeat(autofit,minmax(0,1fr))]
-        min-[700px]:grid-cols-[repeat(2,20rem)] place-content-center gap-8 p-2"
-        >
-          <CardOption>
-            <h2 className="text-white text-2xl">Tú Puedes</h2>
-            <Image src="/card-1.png" alt="card" width={200} height={200} />
-            <Link
-              href=""
-              className="py-2 px-4 mt-2 bg-yellow-app-400 rounded-full text-black text-lg"
-            >
-              Crear Sorteos
-            </Link>
-          </CardOption>
-
-          <CardOption>
-            <h2 className="text-white text-2xl">Lista de sorteos</h2>
-            <Image src="/card-2.png" alt="card" width={300} height={300} />
-
-            <Link
-              href=""
-              className="py-2 px-4 bg-purple-app-400 text-white rounded-full text-lg"
-            >
-              Ver Sorteos
-            </Link>
-          </CardOption>
-        </section>
-      </main>
-    </div> */}
+  
 	return (
     <SorteosContainer moreClass="bg-hero-admin bg-cover bg-[80%_80%]">
       <main className="w-full min-h-[calc(100vh_-_10rem)] flex items-center justify-center p-4">
